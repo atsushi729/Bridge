aceabba7ba80139fc442756ff9affbfe7a383955
Atsushi <atu729@icloud.com>
2024-05-26 19:36:40 +0900

    feat : add Hero component

--- app/routes/gallery+/_index.tsx
+++ app/routes/gallery+/_index.tsx
@@ -1,16 +1,24 @@
 import Background from '#app/components/ui/background'
-import { Button, buttonVariants } from '#app/components/ui/button'
 import Footer from '#app/components/ui/footer'
+import HeroWithImage from '#app/components/ui/hero'
 import Navigation from '#app/components/ui/navigation'
-import { siteConfig } from '#app/utils/constants/brand'
-import { cn } from '#app/utils/misc'
-import { Link } from '@remix-run/react'
+
+const item = {
+  headline: 'hello',
+  subheadline: 'hello',
+  bottomText: {
+    text: 'hello',
+    href: 'https://www.risefor-business.com/file/RISEforBusinesss_service.pdf',
+  },
+}
 
 const Index = () => {
   return (
     <div className="relative flex h-full w-full flex-col bg-card">
       {/* Navigation */}
       <Navigation />
+      {/* Hero */}
+      <HeroWithImage item={item} />
       {/* Footer */}
       <Footer />
       {/* Background */}
