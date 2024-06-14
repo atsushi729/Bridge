03dc4765d3e9578a3843335e7b5cfdf31c198361
Atsushi <atu729@icloud.com>
2024-06-14 23:39:14 +0900

    feat : create stub portfolio data

---
+++ app/constants/portfolio.ts
@@ -0,0 +1,36 @@
+export const portfolio = [
+  {
+    id: 1,
+    name: 'アルゴリズムドキュメント（React + P5.js + Manim）',
+    description:
+      'データ構造や探索・ソートアルゴリズムの概要や計算量をまとめたサイトをReactベースで作成しました。',
+    authorImgUrl: '/images/motivation.jpeg',
+    portfolioImgUr: '/images/algorithm.png',
+    applicationDescription:
+      'このアプリケーションは、データ構造や探索・ソートアルゴリズムの概念を可視化し、学習者がより直感的に理解できるように設計されています。Reactを用いてフロントエンドを構築し、P5.jsとManimを使用してアルゴリズムのアニメーションを実現しています。',
+    techstackDescription:
+      'このアプリケーションの技術スタックには、Reactをメインのフレームワークとして使用し、P5.jsで描画を行い、Manimでアニメーションを作成しています。また、バックエンドにはNode.jsとExpressを使用し、データベースにはMongoDBを採用しています。',
+    techDescription:
+      'このプロジェクトで直面した技術的な課題には、複雑なアルゴリズムのアニメーションの同期、リアルタイムでのユーザー入力処理、パフォーマンスの最適化などがありました。これらの課題を克服するために、非同期処理やWeb Workerを活用し、効率的な描画アルゴリズムを設計しました。',
+    authorName: 'Atsushi Hatakeyama',
+    link: 'https://algorithm-visualizer-bqz.pages.dev/',
+    position: 'Full stack developer',
+  },
+  {
+    id: 2,
+    name: 'アルゴリズムドキュメント（React + P5.js + Manim）',
+    description:
+      'データ構造や探索・ソートアルゴリズムの概要や計算量をまとめたサイトをReactベースで作成しました。',
+    authorImgUrl: '/images/motivation.jpeg',
+    portfolioImgUr: '/images/algorithm.png',
+    applicationDescription:
+      'このアプリケーションは、データ構造や探索・ソートアルゴリズムの概念を可視化し、学習者がより直感的に理解できるように設計されています。Reactを用いてフロントエンドを構築し、P5.jsとManimを使用してアルゴリズムのアニメーションを実現しています。',
+    techstackDescription:
+      'このアプリケーションの技術スタックには、Reactをメインのフレームワークとして使用し、P5.jsで描画を行い、Manimでアニメーションを作成しています。また、バックエンドにはNode.jsとExpressを使用し、データベースにはMongoDBを採用しています。',
+    techDescription:
+      'このプロジェクトで直面した技術的な課題には、複雑なアルゴリズムのアニメーションの同期、リアルタイムでのユーザー入力処理、パフォーマンスの最適化などがありました。これらの課題を克服するために、非同期処理やWeb Workerを活用し、効率的な描画アルゴリズムを設計しました。',
+    authorName: 'Atsushi Hatakeyama',
+    link: 'https://algorithm-visualizer-bqz.pages.dev/',
+    position: 'Full stack developer',
+  },
+]
