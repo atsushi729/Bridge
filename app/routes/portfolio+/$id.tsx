43e627855a4aced3a4a52919f03ee39761053be6
Atsushi <atu729@icloud.com>
2024-06-20 15:07:11 +0900

    refactor : delete stub data

--- app/routes/portfolio+/$id.tsx
+++ app/routes/portfolio+/$id.tsx
@@ -3,41 +3,8 @@ import Background from '#app/components/ui/background'
 import { Button } from '#app/components/ui/button'
 import Footer from '#app/components/ui/footer'
 import Navigation from '#app/components/ui/navigation'
+import { portfolio } from '#app/constants/portfolio'
 import { useNavigate, useParams } from '@remix-run/react'
-
-interface Portfolio {
-  id: number
-  name: string
-  description: string
-  authorImgUrl: string
-  portfolioImgUr?: string
-  applicationDescription: string
-  techstackDescription: string
-  techDescription: string
-  authorName: string
-  link: string
-  position: string
-}
-
-const portfolio: Portfolio[] = [
-  {
-    id: 1,
-    name: 'アルゴリズムドキュメント（React + P5.js + Manim）',
-    description:
-      'データ構造や探索・ソートアルゴリズムの概要や計算量をまとめたサイトをReactベースで作成しました。',
-    authorImgUrl: '/images/avatar.png',
-    portfolioImgUr: '/images/algorithm.png',
-    applicationDescription:
-      'このアプリケーションは、データ構造や探索・ソートアルゴリズムの概念を可視化し、学習者がより直感的に理解できるように設計されています。Reactを用いてフロントエンドを構築し、P5.jsとManimを使用してアルゴリズムのアニメーションを実現しています。',
-    techstackDescription:
-      'このアプリケーションの技術スタックには、Reactをメインのフレームワークとして使用し、P5.jsで描画を行い、Manimでアニメーションを作成しています。また、バックエンドにはNode.jsとExpressを使用し、データベースにはMongoDBを採用しています。',
-    techDescription:
-      'このプロジェクトで直面した技術的な課題には、複雑なアルゴリズムのアニメーションの同期、リアルタイムでのユーザー入力処理、パフォーマンスの最適化などがありました。これらの課題を克服するために、非同期処理やWeb Workerを活用し、効率的な描画アルゴリズムを設計しました。',
-    authorName: 'Atsushi Hatakeyama',
-    link: 'https://algorithm-visualizer-bqz.pages.dev/',
-    position: 'Full stack developer',
-  },
-]
 
 const PortfolioDetail = () => {
   const { id } = useParams<{ id: string }>()
@@ -74,7 +41,7 @@ const PortfolioDetail = () => {
             {/* Main contents */}
             <img
               className="mb-8 h-auto w-full"
-              src={item.portfolioImgUr}
+              src={item.portfolioImgUrl}
               alt={item.name}
             />
             <h2 className="mb-2 font-bold">アプリケーションについて</h2>
